01abb2b56263b85a8b5738738a9e635bfda53a1e
Gonzalo Cáceres <gonzalomauriciocaceresmurga@gmail.com>
2025-09-16 14:50:32 -0500

    Archivo config/env.js actualizado para que tenga validación a errores

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,6 +4,14 @@
 import dotenv from 'dotenv'; //importando el módulo dotenv para trabajar con variables de entorno
 dotenv.config(); //punto de entrada para las variables de entorno
 
+//función para validar si los datos existen en el archivo ".env", si existe lo retornará y lo igualará a la variable correspondiente del objeto, si no existe saltará un error
+function validateEnvVariable(name, value) {
+  if (!value || value.trim() === '') {
+    throw new Error(`La variable de entorno "${name}" está vacía o no definida.`);
+  }
+  return value;
+}
+
 //objeto config que guardará los valores de las variables de entorno
 export const config = {
   server: {
@@ -11,18 +19,18 @@ export const config = {
     env: process.env.NODE_ENV || 'development', //entorno de desarrollo "development" por defecto
   },
   db: {
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
-    host: process.env.DB_HOST,
+    name: validateEnvVariable('DB_NAME', process.env.DB_NAME),
+    user: validateEnvVariable('DB_USER', process.env.DB_USER),
+    pass: validateEnvVariable('DB_PASS', process.env.DB_PASS),
+    host: validateEnvVariable('DB_HOST', process.env.DB_HOST),
   },
   jwt: {
-    accessSecret: process.env.ACCESS_TOKEN_SECRET,
-    refreshSecret: process.env.REFRESH_TOKEN_SECRET,
-    accessExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
-    refreshExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+    accessSecret: validateEnvVariable('ACCESS_TOKEN_SECRET', process.env.ACCESS_TOKEN_SECRET),
+    refreshSecret: validateEnvVariable('REFRESH_TOKEN_SECRET', process.env.REFRESH_TOKEN_SECRET),
+    accessExpiresIn: validateEnvVariable('ACCESS_TOKEN_EXPIRES_IN', process.env.ACCESS_TOKEN_EXPIRES_IN),
+    refreshExpiresIn: validateEnvVariable('REFRESH_TOKEN_EXPIRES_IN', process.env.REFRESH_TOKEN_EXPIRES_IN),
   },
   cors: {
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || null,
   },
 };
