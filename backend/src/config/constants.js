0bae32f4fb0d3b0f4b50f17fc840a6960a2939f8
Gonzalo Cáceres <gonzalomauriciocaceresmurga@gmail.com>
2025-09-16 19:15:22 -0500

    Añadido archivo de constantes, siguiente paso: Hacer los modelos

---
+++ backend/src/config/constants.js
@@ -0,0 +1,9 @@
+// src/config/constants.js
+
+//Este archivo se centra en estandarizar los valores que nunca van a cambiar en el sistema, y tenerlos todos definidos y organizados en un archivo
+
+// Roles de usuario en el sistema
+export const ROLES = Object.freeze({
+  BIBLIOTECARIO: 'bibliotecario',
+  USUARIO: 'cliente',
+});
