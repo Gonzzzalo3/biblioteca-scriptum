272d94b547dee4be52db3e82de121b5361566e69
Gonzalo Cáceres <gonzalomauriciocaceresmurga@gmail.com>
2025-09-16 23:07:48 -0500

    Archivos de modelos creados

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -3,7 +3,7 @@
 //Este archivo se centra en estandarizar los valores que nunca van a cambiar en el sistema, y tenerlos todos definidos y organizados en un archivo
 
 // Roles de usuario en el sistema
-export const ROLES = Object.freeze({
+export const ROLES = Object.freeze({ 
   BIBLIOTECARIO: 'bibliotecario',
   USUARIO: 'cliente',
 });
