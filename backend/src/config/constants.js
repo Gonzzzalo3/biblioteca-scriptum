6f35ac68f09cc6d4f3596fb5e0b807451bdb7551
Gonzalo Cáceres <gonzalomauriciocaceresmurga@gmail.com>
2025-09-16 23:25:15 -0500

    Constantes extendidas

--- backend/src/config/constants.js
+++ backend/src/config/constants.js
@@ -7,3 +7,18 @@ export const ROLES = Object.freeze({
   BIBLIOTECARIO: 'bibliotecario',
   USUARIO: 'cliente',
 });
+
+// Estados de un usuario en el sistema
+export const USER_STATUS = Object.freeze({
+  ACTIVO: 'activo',
+  INACTIVO: 'inactivo',
+  SUSPENDIDO: 'suspendido',
+});
+
+// Estados de un ejemplar en el sistema
+export const EXEMPLARY_STATUS = Object.freeze({
+  DISPONIBLE: 'disponible',
+  PRESTADO: 'prestado',
+  RESERVADO: 'reservado',
+  NO_DISPONIBLE: 'no_disponible',
+});
